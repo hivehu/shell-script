3bd053b6c1d215ac53880490bd2c1af132ad8448
hivehu <38551488+hivehu@users.noreply.github.com>
2019-11-19 09:58:15 +0800

    update the check_orabbix.sh as check.sh

--- check_orabbix.sh
+++ check_orabbix.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 time=`/bin/date '+%F %T'`
-ps -ef | grep orabbix | grep -v grep > /dev/null
+#n=`ps -ef | grep orabbix | grep -v grep | wc -l
+echo -e "\n$time"
+/usr/bin/ps -ef | /usr/bin/grep orabbix | /usr/bin/grep -v 'grep' > /dev/null
 if [ $? -eq 0 ]
 then
-        echo -e "\n$time"
+#        echo -e "\n$time"
         echo "the orabbix is running"
+#       /opt/orabbix/init.d/orabbix start &
         exit 0
 else
-        echo -e "\n$time"
+#        echo -e "\n$time"
         echo "the orabbix is dead"
         /opt/orabbix/init.d/orabbix start &
 fi
